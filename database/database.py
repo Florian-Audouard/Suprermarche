1a91f8faa50973e22ddb06c74442ff8debd268d1
Florian-Audouard <nairolf.audouard@gmail.com>
2024-10-17 10:21:09 +0200

    pylint friendly

--- database/database.py
+++ database/database.py
@@ -1,9 +1,8 @@
-"""_summary_
-
-Returns:
-    _type_: _description_
-"""
-
+#pylint:disable=missing-module-docstring
+#pylint:disable=missing-function-docstring
+#pylint:disable = no-value-for-parameter
+#pylint:disable=not-context-manager
+#pylint:disable=trailing-whitespace
 import os
 from random import choice, randint, sample
 import urllib.parse
@@ -24,52 +23,52 @@ if os.path.exists(ENV_FILENAME):
     config = dotenv_values(ENV_FILENAME)
 else:
     config = {
-        "USER": os.environ.get("USER_DB"),
-        "PASSWORD": os.environ.get("PASSWORD_DB"),
-        "HOST": os.environ.get("HOST_DB"),
-        "PORT": os.environ.get("PORT_DB"),
-        "DATABASE": os.environ.get("DATABASE_DB"),
+        "USER":os.environ.get("USER_DB"),
+        "PASSWORD":os.environ.get("PASSWORD_DB"),
+        "HOST":os.environ.get("HOST_DB"),
+        "PORT":os.environ.get("PORT_DB"),
+        "DATABASE":os.environ.get("DATABASE_DB"),
     }
 FILENAME_TAB = ["database.sql", "function.sql", "trigger.sql"]
 
 FILENAME_DB_DONNE = "donne_test.sql"
 options = urllib.parse.quote_plus("--search_path=modern,public")
-CONN_PARAMS = f"postgresql://{config['USER']}:{config['PASSWORD']}@{config['HOST']}:{config['PORT']}/{config['DATABASE']}?options={options}"  # pylint: disable=line-too-long
+CONN_PARAMS = f"postgresql://{config['USER']}:{config['PASSWORD']}@{config['HOST']}:{config['PORT']}/{config['DATABASE']}?options={options}"  # pylint:disable=line-too-long
 
 
 def clean_querry(func):
     def wrapper_func(*args, **kwargs):
         res = ""
-        postgres = psycopg2.connect(CONN_PARAMS)
+        conn = psycopg2.connect(CONN_PARAMS)
         try:
-            with postgres as conn:  # pylint: disable=not-context-manager
+            with conn:
                 with conn.cursor() as cur:
                     res = func(cur,*args, **kwargs)
         finally:
-            postgres.close()
+            conn.close()
         return res
     return wrapper_func
 
 @clean_querry
-def reset_table(cur):  # pylint: disable=missing-function-docstring
+def reset_table(cur): 
     for filename in FILENAME_TAB:
         with open(filename, "r", encoding="utf-8") as file:
             cur.execute(file.read())
 
 @clean_querry
-def get_data(cur):  # pylint: disable=missing-function-docstring
+def get_data(cur):
     cur.execute(
         "select Nom_produit,Marque,description,prix,quantite from Stock_Quantite;"
     )
     return cur.fetchall()
 
 @clean_querry
-def init_data(cur):  # pylint: disable=missing-function-docstring
+def init_data(cur):
     with open(FILENAME_DB_DONNE, "r", encoding="utf-8") as file:
         cur.execute(file.read())
 
 @clean_querry
-def add_produit(cur):  # pylint: disable=missing-function-docstring
+def add_produit(cur):
     cur.execute("""SELECT Num_Description FROM Description;""")
     list_produit = cur.fetchall()
     list_produit = [x[0] for x in list_produit]
@@ -77,18 +76,18 @@ def add_produit(cur):  # pylint: disable=missing-function-docstring
         cur.execute(
             """SELECT add_stock_init(%(Num_Produit)s);""",
             {
-                "Num_Produit": produit,
+                "Num_Produit":produit,
             },
         )
 
-def achat(cur,client, paiement, list_achat):  # pylint: disable=missing-function-docstring
+def achat(cur,client, paiement, list_achat): 
     cur.execute(
         """SELECT transaction(%(client)s,%(paiement)s,%(liste_achat)s);""",
-        {"client": client, "paiement": paiement, "liste_achat": list_achat},
+        {"client":client, "paiement":paiement, "liste_achat":list_achat},
     )
 
 @clean_querry
-def add_transaction(cur):  # pylint: disable=missing-function-docstring
+def add_transaction(cur):
     cur.execute("""SELECT num_client FROM client; """)
     tmp = cur.fetchall()
     list_cli = [x[0] for x in tmp]
@@ -114,7 +113,7 @@ def add_transaction(cur):  # pylint: disable=missing-function-docstring
 
 #permet d'obtenir toutes les informations du clients or mdp et login
 @clean_querry
-def get_profil(cur): # pylint: disable=missing-function-docstring
+def get_profil(cur):
     cur.execute(
         "select Num_Client, Nom, Prenom, Pt_Fidelite, Age,Mail,Num_tel from Client;"
     )
@@ -122,7 +121,7 @@ def get_profil(cur): # pylint: disable=missing-function-docstring
             
 #permet d'obtenir tous les articles disponibles en magasin
 @clean_querry
-def get_stockdispo(cur): # pylint: disable=missing-function-docstring
+def get_stockdispo(cur):
     cur.execute(
         "select * from Stock_Quantite_Disponible;"
     )
@@ -130,7 +129,7 @@ def get_stockdispo(cur): # pylint: disable=missing-function-docstring
 
 #permet d'obtenir toutes les infos des stocks
 @clean_querry
-def get_allstock(cur): # pylint: disable=missing-function-docstring
+def get_allstock(cur):
     cur.execute(
         "select * from Stock_Quantite;"
     )
@@ -138,15 +137,15 @@ def get_allstock(cur): # pylint: disable=missing-function-docstring
 
 #permet l'historique des achats := ligneachat
 @clean_querry
-def get_historique(cur): # pylint: disable=missing-function-docstring
+def get_historique_data(cur):
     cur.execute(
         "select * from Ligne_Achat;"
     )
     return cur.fetchall()
-            
+           
 #permet d'obtenir le detail de l'historique
 @clean_querry
-def get_detailhist(cur): # pylint: disable=missing-function-docstring
+def get_detail_historique(cur):
     cur.execute(
         "SELECT * FROM Client_Produit;"
     )
@@ -154,7 +153,7 @@ def get_detailhist(cur): # pylint: disable=missing-function-docstring
 
 #permet d'obtenir tout le stock vendu
 @clean_querry
-def get_allstockvendu(cur): # pylint: disable=missing-function-docstring
+def get_all_stock_vendu_data(cur):
     cur.execute(
         """SELECT * 
         FROM Produit_in_stock 
--- server.py
+++ server.py
@@ -1,3 +1,10 @@
+#pylint:disable=missing-module-docstring
+#pylint:disable=missing-function-docstring
+#pylint:disable = no-value-for-parameter
+#pylint:disable=not-context-manager
+#pylint:disable=trailing-whitespace
+#pylint:disable=wrong-import-position
+#pylint:disable=line-too-long
 import os
 from flask import Flask, jsonify, render_template
 
@@ -6,7 +13,7 @@ os.chdir(os.path.dirname(__file__))
 app = Flask(__name__)
 
 # Si il est mis plus haut la page crash
-from database.database import *
+from database.database import get_data,get_profil,get_stockdispo,get_allstock,get_historique_data,get_detail_historique,get_all_stock_vendu_data
 
 
 @app.route("/")
@@ -19,28 +26,28 @@ def get_database():  # pylint: disable=missing-function-docstring
     return jsonify({"table": get_data()})
 
 @app.route("/getProfilClient", methods=["GET"])
-def get_ProfilClient():  # pylint: disable=missing-function-docstring
+def get_profil_client():  # pylint: disable=missing-function-docstring
     return jsonify({"table": get_profil()})
 
 @app.route("/getStockDispo", methods=["GET"])
-def get_StockDispo():  # pylint: disable=missing-function-docstring
+def get_stock_dispo():  # pylint: disable=missing-function-docstring
     return jsonify({"table": get_stockdispo()})
 
 @app.route("/getAllStock", methods=["GET"])
-def get_AllStock():  # pylint: disable=missing-function-docstring
+def get_all_stock():  # pylint: disable=missing-function-docstring
     return jsonify({"table": get_allstock()})
     
 @app.route("/getHistorique",methods=["GET"])
-def get_Historique():  # pylint: disable=missing-function-docstring
-    return jsonify({"table": get_historique()})
+def get_historique():  # pylint: disable=missing-function-docstring
+    return jsonify({"table": get_historique_data()})
 
 @app.route("/getDetailsHisto",methods=["GET"])
-def get_DetailHist():  # pylint: disable=missing-function-docstring
-    return jsonify({"table": get_detailhist()})
+def get_detail_hist():  # pylint: disable=missing-function-docstring
+    return jsonify({"table": get_detail_historique()})
 
 @app.route("/getAllStockVendu",methods=["GET"])
-def get_AllStockVendu():  # pylint: disable=missing-function-docstring
-    return jsonify({"table": get_allstockvendu()})
+def get_all_stock_vendu():  # pylint: disable=missing-function-docstring
+    return jsonify({"table": get_all_stock_vendu_data()})
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
